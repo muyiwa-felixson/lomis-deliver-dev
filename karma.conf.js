666ba77a0fb134eb5903c9f2a654d97cd15c903b
Rukayat Sadiq <fruqoyya@gmail.com>
2017-03-15 02:43:19 +0100

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,24 @@ const webpackConfig = require('./webpack/webpack.config.js');
 
 module.exports = function (config) {
   config.set({
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
+    },
     browsers: ['Chrome'], // run in Chrome
     singleRun: true, // just run once by default
     frameworks: ['mocha', 'chai-sinon'], // use the mocha test framework
     files: [
       'tests.webpack.js', // Test files
+    ],
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-coverage',
+      'karma-jasmine-html-reporter',
+      'karma-ng-html2js-preprocessor',
     ],
     preprocessors: {
       'tests.webpack.js': ['webpack', 'sourcemap'], // preprocess with webpack and our sourcemap loader
@@ -21,4 +34,8 @@ module.exports = function (config) {
       dir: 'coverage',
     },
   });
+
+  if (process.env.TRAVIS) {
+    config.browsers = ['Chrome_travis_ci'];
+  }
 };
