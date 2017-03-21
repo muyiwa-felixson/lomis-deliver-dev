28b710956c4a30d4a6ab95eb888f987d24548064
Rukayat Sadiq <fruqoyya@gmail.com>
2017-03-21 22:23:04 +0100

    create dashboard components

--- src/components/dashboard-title/dashboard-title.jsx
+++ src/components/dashboard-title/dashboard-title.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { DateRange } from 'components';
 
-export default class DashboardTitle extends Component {
+class DashboardTitle extends Component {
   state = {};
 
   render() {
@@ -8,37 +9,43 @@ export default class DashboardTitle extends Component {
       <div className="dash-filter row">
         <div className="col-md-5 col-sm-12">
           <div className="dash-title">
-            <h4><span>Delivery Round:</span><strong>BA-20-2016</strong></h4>
-            <span>Delivery Type:</span><strong>Monthly, Bi-monthly</strong>
+            <h4>
+              <span>Delivery Round:</span>
+              <strong>{this.props.round.round.id}</strong>
+            </h4>
+            <span>Delivery Type:</span><strong>Monthly</strong>
           </div>
         </div>
 
         <div className="col-md-7 col-sm-12">
-          <div className="col-md-4 col-sm-6 filter-input-case">
-            <div className="inner-addon right-addon">
-              <i className="icon icon-marker" />
-              <input type="text" className="form-control radius-secondary" />
+          <div className="row">
+            <div className="col-md-4 col-sm-6 filter-input-case">
+              <div className="inner-addon right-addon">
+                <i className="icon icon-marker" />
+                <input type="text" className="form-control radius-secondary" />
+              </div>
             </div>
-          </div>
-          <div className="col-md-4 col-sm-6 filter-input-case">
-            <div className="inner-addon right-addon inner-select radius-secondary">
-              <select className="form-control radius-secondary">
-                <option>-Select Round-</option>
-                <option>Round 1</option>
-                <option>Round 2</option>
-                <option>Round 3</option>
-              </select>
-              <i className="glyphicon glyphicon-triangle-bottom" />
+            <div className="col-md-4 col-sm-6 filter-input-case">
+              <div className="inner-addon overflow right-addon inner-select radius-secondary">
+                <select className="form-control radius-secondary">
+                  <option>-Select Round-</option>
+                  <option>Round 1</option>
+                  <option>Round 2</option>
+                  <option>Round 3</option>
+                </select>
+                <i className="icon icon-caret-down" />
+              </div>
             </div>
-          </div>
-          <div className="col-md-4 col-sm-6 filter-input-case">
-            <div className="inner-addon left-addon">
-              <i className="glyphicon glyphicon-calendar" />
-              <input type="date" className="form-control radius-secondary" />
-            </div>
+            <DateRange />
           </div>
         </div>
       </div>
     );
   }
 }
+
+DashboardTitle.propTypes = {
+  round: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+export default DashboardTitle;
---
+++ src/components/delivery-count/delivery-card/delivery-card.jsx
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react';
+
+class DeliveryCard extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="col-md-3 col-sm-6">
+        <div className="dash-summary radius-primary row">
+          <div className={`col-xs-4 col-md-4 col-lg-4 micon ${this.props.color}`}>
+            <i className={`icon icon-${this.props.icon}`} />
+          </div>
+          <div className="col-xs-8 col-md-8 col-lg-8 mdet">
+            <h3>{this.props.count}</h3>
+            <span className={`c${this.props.color}`}>
+              {this.props.type}
+            </span>
+            Delivery
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+DeliveryCard.propTypes = {
+  icon: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  count: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
+export default DeliveryCard;
