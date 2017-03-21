94009d2e42ae1d3b6893c4e645a8ef5e0fe99482
Rukayat Sadiq <fruqoyya@gmail.com>
2017-03-21 11:08:26 +0100

    update config with urls

--- src/config/development.js
+++ src/config/development.js
@@ -1,9 +1,9 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8055';
 
 const config = {
   USER_URL: `${BASE_URL}/v1/users/me`,
   AUTH_URL: `${BASE_URL}/v1/auth`,
+  ROUND_URL: `${BASE_URL}/v1/rounds`,
 };
 
-
-export default config;
+module.exports = config;
