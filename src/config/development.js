27a11068d90201588be810e3914d8d343b7413fb
Mofesola <me@mofesola.com>
2017-03-29 17:01:37 +0100

    Add base production URL

--- src/config/development.js
+++ src/config/development.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://lomis-deliver-api-dev.ehealthafrica.org';
 
 const config = {
   USER_URL: `${BASE_URL}/v1/users/me`,
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV || 'developement';
+const env = process.env.NODE_ENV || 'development';
 
 const config = require(`./${env}.js`); // eslint-disable-line import/no-dynamic-require
 
