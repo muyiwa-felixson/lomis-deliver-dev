23b305c5046780e0eb03b4657d2e1666ae9c33ed
Rukayat Sadiq <fruqoyya@gmail.com>
2017-03-30 09:51:07 +0100

    set api url for production

--- src/config/production.js
+++ src/config/production.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.HOST || 'https://lomis-deliver-api-dev.ehealthafrica.org';
+const BASE_URL = 'https://lomis-deliver-api-dev.ehealthafrica.org';
 
 const config = {
   USER_URL: `${BASE_URL}/v1/users/me`,
