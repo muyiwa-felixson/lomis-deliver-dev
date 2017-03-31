b2eaf21de8ee863caca6911d43a67f41f1138c5e
Rukayat Sadiq <fruqoyya@gmail.com>
2017-03-31 03:19:58 +0100

    correct date implementation for round progress

--- src/components/progress-bar/progress-bar.jsx
+++ src/components/progress-bar/progress-bar.jsx
@@ -3,7 +3,30 @@ import React, { Component, PropTypes } from 'react';
 class ProgressBar extends Component {
   state = {};
 
+  checkProgressStatus = () => { // eslint-disable-line consistent-return
+    const today = new Date();
+    const date = this.props.roundDetails.doc.endDate;
+
+    if (today.getTime() > Date.parse(date)) {
+      return 'complete round';
+    } else if (today.getTime() === Date.parse(date)) {
+      return 'just completed';
+    } else if (today.getTime() < Date.parse(date)) {
+      return 'incomplete round';
+    }
+  }
+
+  handleDate = () => {
+    const today = new Date();
+    return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  }
+
   render() {
+    const roundDetails = this.props.roundDetails;
+    const status = this.props.status[0].value;
+    const complete = (status.completedDeliveries / status.totalDeliveries) * 100;
+    const position = complete + ((status.cancelledDeliveries / status.totalDeliveries) * 100);
+
     return (
       <div className="page-content row radius-primary round-progress-box">
         <div>
@@ -11,15 +34,21 @@ class ProgressBar extends Component {
         </div>
         <div className="round-progress">
           <div className="bar grey">
-            <div className="bar-day" style={{ width: `${this.props.position}%` }} />
-            <div className="bar-complete" style={{ width: `${this.props.complete}%` }} />
+            { this.checkProgressStatus() === 'incomplete round' ?
+              <div className="bar-day" style={{ width: `${position}%` }} /> :
+              <div className="bar-day" style={{ width: '100%' }} />
+            }
+            <div className="bar-complete" style={{ width: `${complete}%` }} />
           </div>
           <div className="progress-date">
-            <span className="span-left"><strong>Start</strong><br />02/02/17</span>
-            <span className="span-right"><strong>End</strong><br />02/05/17</span>
-            <div className="progress-day" style={{ width: `${this.props.position}%` }}>
-              <span><strong>Today</strong><br />20/02/17</span>
-            </div>
+            <span className="span-left"><strong>Start</strong><br />{roundDetails.doc.startDate}</span>
+            <span className="span-right"><strong>End</strong><br />{roundDetails.doc.endDate}</span>
+            { this.checkProgressStatus() !== 'incomplete round' ?
+              <div className="progress-day" style={{ width: '100%' }} /> :
+              <div className="progress-day" style={{ width: `${position}%` }}>
+                <span><strong>Today</strong><br />{ this.handleDate() }</span>
+              </div>
+            }
           </div>
         </div>
       </div>
@@ -28,8 +57,8 @@ class ProgressBar extends Component {
 }
 
 ProgressBar.propTypes = {
-  position: PropTypes.string.isRequired,
-  complete: PropTypes.string.isRequired,
+  status: PropTypes.array, // eslint-disable-line react/forbid-prop-types
+  roundDetails: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 };
 
 export default ProgressBar;
