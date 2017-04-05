412f2c3dbabfef99893bea8ef5adbb45613755ab
Rukayat Sadiq <fruqoyya@gmail.com>
2017-04-05 22:38:36 +0100

    add components for location and round filters

--- src/components/dashboard-title/dashboard-title.jsx
+++ src/components/dashboard-title/dashboard-title.jsx
@@ -1,10 +1,56 @@
 import React, { Component, PropTypes } from 'react';
 import { DateRange } from 'components';
+import Select from 'react-select';
+import Api from 'helpers/api';
+import config from 'config';
+import { connect } from 'react-redux';
+import { fetchSingleRound, fetchRoundCount } from 'redux/actions/roundActions';
+
+import 'react-select/scss/default.scss';
 
 class DashboardTitle extends Component {
-  state = {};
+  state = {
+    locationValue: '',
+    roundValue: '',
+    roundResult: [],
+  };
+
+  getRound = (val) => {
+    this.setState({
+      roundValue: val.value,
+    });
+    this.props.singleRound(val.value);
+    this.props.roundCount(val.value);
+  }
+
+  updateLocation = (val) => {
+    const apiClient = new Api();
+    apiClient.get(`${config.ROUND_LOCATION_URL}${val}`)
+      .then((res) => {
+        const result = res.map((round) => {
+          const roundObj = {};
+          roundObj.value = round.id;
+          roundObj.label = round.id;
+
+          return roundObj;
+        });
+        this.setState({ roundResult: result });
+      });
+    this.setState({
+      locationValue: val,
+    });
+  }
 
   render() {
+    const opt = this.props.locations !== undefined ?
+    this.props.locations.locations.map((location) => {
+      const obj = {};
+      obj.value = location.key;
+      obj.label = location.key;
+
+      return obj;
+    }) : '';
+
     return (
       <div className="dash-filter row">
         <div className="col-md-5 col-sm-12">
@@ -22,18 +68,12 @@ class DashboardTitle extends Component {
             <div className="col-md-4 col-sm-6 filter-input-case">
               <div className="inner-addon right-addon">
                 <i className="icon icon-marker" />
-                <input type="text" className="form-control radius-secondary" />
+                <Select name="selected-location" placeholder="Select Location..." options={opt} simpleValue value={this.state.locationValue} onChange={this.updateLocation} />
               </div>
             </div>
             <div className="col-md-4 col-sm-6 filter-input-case">
-              <div className="inner-addon overflow right-addon inner-select radius-secondary">
-                <select className="form-control radius-secondary">
-                  <option>-Select Round-</option>
-                  <option>Round 1</option>
-                  <option>Round 2</option>
-                  <option>Round 3</option>
-                </select>
-                <i className="icon icon-caret-down" />
+              <div className="inner-addon right-addon">
+                <Select name="selected-round" placeholder="Select Delivery Round..." options={this.state.roundResult} value={this.state.roundValue} onChange={this.getRound} />
               </div>
             </div>
             <DateRange />
@@ -46,6 +86,14 @@ class DashboardTitle extends Component {
 
 DashboardTitle.propTypes = {
   round: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  locations: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  singleRound: PropTypes.func.isRequired,
+  roundCount: PropTypes.func.isRequired,
 };
+const mapStatetoProps = ({ rounds, locations }) => ({ round: rounds, locations });
 
-export default DashboardTitle;
+export default connect(mapStatetoProps,
+  {
+    singleRound: fetchSingleRound,
+    roundCount: fetchRoundCount,
+  })(DashboardTitle);
