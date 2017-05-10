a522fc3a0eca52eeeff29b7cdefa0a51d30f71e1
Rukayat Sadiq <fruqoyya@gmail.com>
2017-05-10 12:39:11 +0100

    rename helper and add chronological date validation

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -5,6 +5,13 @@ const roundHelper = {
     const patt = new RegExp('/spreadsheets/d/');
     return (a.host && patt.test(a.pathname));
   },
+
+  checkModalDate: (sd, ed) => {
+    const startDate = new Date(sd);
+    const endDate = new Date(ed);
+
+    return Date.parse(endDate) > Date.parse(startDate);
+  },
 };
 
 export default roundHelper;
